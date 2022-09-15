96e0331d17b2936d0f55de17da781392709df247
tongoonamujera <tongoonamujera@gmail.com>
2022-09-15 15:37:23 +0200

    Add support for styles

---
+++ src/Reactive.js
@@ -0,0 +1,135 @@
+const createElement = (type,  attributes = {}, ...children) => {
+  const element = document.createElement(type);
+  Object.keys(attributes || {}).forEach(key => {
+    (key === "style" ? (
+      Object.keys(attributes[key]).forEach(attr => {
+        element.style[attr] = attributes[key][attr];
+      })
+    )
+      : 
+    (element[key] = attributes[key]))
+  })
+
+  const processChildren = (child) => {
+    (Array.isArray(child) ? (
+      child.forEach(sibling => processChildren(sibling))
+    ) : (typeof child === 'object') ? (
+      element.appendChild(child)
+    )
+        : element.appendChild(document.createTextNode(child))
+    )
+  }
+
+  (children || []).forEach(ch => processChildren(ch));
+
+  return element;
+}
+
+let globalId = 0;
+let componentState = new Map();
+let globalParent;
+
+const useState = (initialState) => {
+  const id = globalId;
+  const parent = globalParent;
+  globalId++;
+  return (() => {
+    const { stateData} = componentState.get(parent);
+  
+      if (stateData[id] === null ) 
+        (stateData[id] = { value: (typeof initialState === 'function' ? initialState() : initialState ) })
+  
+    const setState = state => {
+      const {props, element } =  componentState.get(parent);
+      (typeof state === 'function' ?
+        (stateData[id].value = state(stateData[id].value))
+        : (stateData[id].value = state)
+        )
+  
+      renderToDom(element, props, parent);
+    }
+    return [stateData[id].value, setState];
+  })()
+}
+
+const useEffect = (callbackFn, deps) => {
+  const id = globalId;
+  const parent = globalParent;
+  globalId++;
+  (() => {
+    const { stateData } = componentState.get(parent);
+  
+    if (stateData[id] === null)
+      (stateData[id] = { deps: undefined })
+
+    const depsChanged = deps == null || deps.some(
+      (dep, i) => {
+        return stateData[id].deps == null || stateData[id].deps[i] !== dep
+    })
+
+    if (depsChanged) {
+      if (stateData[id] != null) stateData[id].cleanup()
+      stateData[id].cleanup = callbackFn()
+      stateData[id].deps = deps;
+    }
+  })()
+}
+
+const useMemo = (callbackFn, deps) => {
+  const id = globalId;
+  const parent = globalParent;
+  globalId++;
+  return (() => {
+    const { stateData } = componentState.get(parent);
+  
+    if (stateData[id] === null)
+      (stateData[id] = { deps: undefined })
+
+    const depsChanged = deps == null || deps.some(
+      (dep, i) => {
+        return stateData[id].deps == null || stateData[id].deps[i] !== dep
+    })
+
+    if (depsChanged) {
+      stateData[id].value = callbackFn();
+      stateData[id].deps = deps;
+    }
+
+    return stateData[id].value
+  })()
+}
+
+const renderToDom = (element, props, container) => {
+  let state = componentState.get(container) || { stateData: [] }
+  componentState.set(container, { ...state, element, props });
+  globalParent = container;
+  globalId = 0;
+  (container && element) && (container.appendChild(element))
+}
+
+
+const ReactiveJs = (function () {
+  return {
+    renderToDom,
+    createElement,
+  }
+})()
+
+
+
+/** @jsx ReactiveJs.createElement */
+
+
+console.log(ReactiveJs.createElement)
+
+
+const container = document.querySelector(".root");
+console.log("container: ", container);
+
+ReactiveJs.renderToDom((
+<div style={{fontWeight: "bold"}}>
+    hey thats fucking boring
+  </div>
+),
+  {},
+  container);
---
+++ src/index.js
@@ -0,0 +1,1 @@
+import ReactiveJs from "./Reactive.js";
